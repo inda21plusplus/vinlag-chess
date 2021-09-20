12c31d60da858594ffa4b9d556a5820176b5e009
Vincent <vinlag@kth.se>
2021-09-20 18:24:19 +0200

    fixed illegal moves

--- src/lib.rs
+++ src/lib.rs
@@ -102,29 +102,171 @@ mod tests {
         }
         return (num_moves, time);
     }
-    
+
     #[test]
-    fn run_deep_test_undo() {
+    fn run_deep_test_1() {
         let game = get_board(STANDARD_BOARD.to_string()).unwrap();
         //println!("TIME {}ms", move_all(game, 3).1 / 1_000_000);
         assert_eq!(move_all(game, 0).0, 1);
         assert_eq!(move_all(game, 1).0, 20);
         assert_eq!(move_all(game, 2).0, 400);
         assert_eq!(move_all(game, 3).0, 8902);
-        assert_eq!(move_all(game, 4).0, 197281);
 
+        //assert_eq!(move_all(game, 4).0, 197281);
         //627.90s and it works!!
         //assert_eq!(move_all(game, 5, 0).0, 4865609);
     }
 
     #[test]
     fn run_deep_test_2() {
-        let game = get_board("rnbqkbnr/ppp1p2p/6p1/3p1p2/6P1/5P1N/PPPPP1BP/RNBQK2R b KQkq - 1 4".to_string()).unwrap();
-        //println!("TIME {}ms", move_all(game, 3).1 / 1_000_000);
-       // assert_eq!(move_all(game, 0).0, 1);
+        let game = get_board(
+            "rnbqkbnr/ppp1p2p/6p1/3p1p2/6P1/5P1N/PPPPP1BP/RNBQK2R b KQkq - 1 4".to_string(),
+        )
+        .unwrap();
+        assert_eq!(move_all(game, 0).0, 1);
         assert_eq!(move_all(game, 1).0, 27);
         assert_eq!(move_all(game, 2).0, 671);
-       // assert_eq!(move_all(game, 3).0, 8902);
-       // assert_eq!(move_all(game, 3).0, 19218);
+        assert_eq!(move_all(game, 3).0, 19218);
+    }
+
+    #[test]
+    fn run_deep_test_3() {
+        let game = get_board(
+            "r1bqkb1r/p1pppppp/8/1p4B1/2Pn2n1/N3Q3/PP2PPPP/R3KBNR b KQkq - 1 6".to_string(),
+        )
+        .unwrap();
+        assert_eq!(move_all(game, 0).0, 1);
+        assert_eq!(move_all(game, 1).0, 32);
+        assert_eq!(move_all(game, 2).0, 1173);
+        assert_eq!(move_all(game, 3).0, 36787);
+    }
+
+    #[test]
+    fn temp_test_invalid_2() {
+        let list2 = vec![
+            ("b2b3", 32),
+            ("f2f3", 32),
+            ("g2g3", 32),
+            ("h2h3", 32),
+            ("c4c5", 30),
+            ("b2b4", 31),
+            ("f2f4", 32),
+            ("h2h4", 32),
+            ("c4b5", 31),
+            ("g1f3", 32),
+            ("g1h3", 32),
+            ("a3b1", 32),
+            ("a3c2", 32),
+            ("a3b5", 31),
+            ("g5f4", 32),
+            ("g5h4", 32),
+            ("g5f6", 33),
+            ("g5h6", 31),
+            ("a1b1", 32),
+            ("a1c1", 32),
+            ("a1d1", 32),
+            ("e3c1", 32),
+            ("e3d2", 32),
+            ("e3b3", 32),
+            ("e3c3", 32),
+            ("e3d3", 32),
+            ("e3f3", 32),
+            ("e3g3", 32),
+            ("e3h3", 32),
+            ("e3d4", 25),
+            ("e3e4", 32),
+            ("e3f4", 32),
+            ("e3e5", 32),
+            ("e3e6", 30),
+            ("e3e7", 3),
+            ("e1d1", 32),
+            ("e1d2", 32),
+            ("e1c1", 32),
+        ];
+        let fen2 = "r1bqkb1r/p1ppp1pp/5p2/1p4B1/2Pn2n1/N3Q3/PP2PPPP/R3KBNR w KQkq - 0 7";
+        let dep2 = 2;
+
+        for q in list2 {
+            let mut game = get_board(fen2.to_string()).unwrap();
+            let temp_move = parse_move(q.0).unwrap();
+            move_piece_no_map(&mut game, temp_move.0, temp_move.1, true);
+
+            let moves = move_all(game, dep2 - 1);
+            if moves.0 != q.1 {
+                //let board2 = print_board(&game).unwrap(); //print_board(&game).unwrap();
+                // println!(">>> {}", board2);
+                // let game2 = get_board(board2).unwrap();
+                // let moves2 = move_all(game2, 1);
+                println!("{} / {} at {}", moves.0, q.1, q.0);
+            }
+        }
+    }
+
+    #[test]
+    fn temp_test_invalid_1() {
+        let list = vec![("f8e7", 1), ("d8e7", 1), ("e8e7", 1)];
+        let fen = "r1bqkb1r/p1ppQ1pp/5p2/1p4B1/2Pn2n1/N7/PP2PPPP/R3KBNR b KQkq - 0 7";
+
+        let game2 = get_board(fen.to_string()).unwrap();
+        let threatmap2 = generate_all_threats(&game2, !game2.is_white_to_move);
+
+        let mut size = 0;
+        for x in 0..BOARD_SIZE {
+            for y in 0..BOARD_SIZE {
+                let pos = Position { x: x, y: y };
+                if is_square_color(&game2, &pos, game2.is_white_to_move) {
+                    let moves = generate_valid_moves(&game2, &threatmap2, &pos);
+                    for m_move in moves {
+                        size += 1;
+                        let q = print_move(pos, m_move);
+                        let mut contains_q = false;
+                        for z in list.clone() {
+                            if z.0 == q {
+                                contains_q = true;
+                            }
+                        }
+                        if !contains_q {
+                            let threatmap = generate_all_threats(&game2, !game2.is_white_to_move);
+                            let all_moves = generate_valid_moves(&game2, &threatmap, &pos);
+                            let t_moves = generate_valid_moves(&game2, &threatmap, &pos);
+                            crate::render::render_highlight(
+                                &game2,
+                                vec![
+                                    (&threatmap.all_threats, termcolor::Color::Red),
+                                    // (&threatmap.all_threats, termcolor::Color::Red),
+                                ],
+                            );
+                            println!("ILEGAL MOVE: {}", q);
+                        }
+                    }
+                }
+            }
+        }
+
+        if size != list.len() {
+            println!("QQ MOVE: {}", size);
+        }
+
+        for q in list.clone() {
+            let mut game = get_board(fen.to_string()).unwrap();
+            let temp_move = parse_move(q.0).unwrap();
+
+            let flags = move_piece_no_map(&mut game, temp_move.0, temp_move.1, true);
+
+            if flags.contains(&MoveFlags::Invalid) {
+                let threatmap = generate_all_threats(&game, !game.is_white_to_move);
+                let all_moves = generate_all_moves_and_castle(&game, &threatmap, &temp_move.0);
+                let moves = generate_valid_moves(&game, &threatmap, &temp_move.0);
+                crate::render::render_highlight(
+                    &game,
+                    vec![
+                        //(&all_moves, termcolor::Color::Blue),
+                        (&threatmap.all_king_threats_full, termcolor::Color::Red),
+                    ],
+                );
+                println!("Did not catch move {}", q.0);
+                break;
+            }
+        }
     }
 }
--- src/logic.rs
+++ src/logic.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-use crate::{game_data::*, parser::print_position};
+use crate::game_data::*;
 
 fn get_position(pos: &Position, offset: &Vector2) -> Option<Position> {
     let new_position = Vector2 {
@@ -26,7 +26,7 @@ fn is_square_none(game: &Game, piece_position: &Position) -> bool {
     return game.board[piece_position.x][piece_position.y].piece == Piece::None;
 }
 
-fn is_square_color(game: &Game, piece_position: &Position, is_white: bool) -> bool {
+pub(crate) fn is_square_color(game: &Game, piece_position: &Position, is_white: bool) -> bool {
     let piece_data = game.board[piece_position.x][piece_position.y];
     return piece_data.piece != Piece::None && piece_data.is_white == is_white;
 }
@@ -185,7 +185,7 @@ pub(crate) fn generate_all_moves(game: &Game, piece_position: &Position) -> Thre
                         // break if the piece is of another color, because the pawn cant ghost though pieces
                         let capture_piece = game.board[new_valid_position.x][new_valid_position.y];
                         if capture_piece.piece != Piece::None
-                            && (capture_piece.is_white != start_piece.is_white)
+                        //&& (capture_piece.is_white != start_piece.is_white)
                         {
                             // as we are only intrested in the first 2 or king, the rest can go
                             if capture_piece.piece == Piece::King {
@@ -220,6 +220,9 @@ pub(crate) fn generate_all_moves(game: &Game, piece_position: &Position) -> Thre
                             }
                         }
                     } else {
+                        if ghost_index == 0 {
+                            all_threats.insert(new_valid_position);
+                        }
                         break;
                     }
                 }
@@ -362,6 +365,7 @@ pub fn generate_all_threats(game: &Game, is_white: bool) -> ThreatMap {
 
 fn get_castle_positions(
     game: &Game,
+    other_team_threat_map: &ThreatMap,
     piece_position: &Position,
     is_white: bool,
 ) -> HashSet<Position> {
@@ -389,12 +393,13 @@ fn get_castle_positions(
 
     // right y pos
     if spawn_y == piece_position.y {
-        let other_threats = generate_all_threats(game, !is_white);
-
         // king cant castle if checked
         const KING_SIDES: [i8; 2] = [-1, 1];
         for offset in KING_SIDES {
-            if !other_threats.all_king_threats.contains(piece_position) {
+            if !other_team_threat_map
+                .all_king_threats
+                .contains(piece_position)
+            {
                 let is_king_side = offset > 0;
 
                 // if can castle
@@ -423,7 +428,9 @@ fn get_castle_positions(
 
                         // the king cant be checked on his way over to the rook,
                         // because the king only moves 2 squares, the index is 2
-                        if other_threats.all_king_threats.contains(&new_valid_position)
+                        if other_team_threat_map
+                            .all_threats
+                            .contains(&new_valid_position)
                             && index <= 2
                         {
                             break;
@@ -459,7 +466,11 @@ fn get_castle_positions(
     return moves;
 }
 
-pub fn generate_all_moves_and_castle(game: &Game, piece_position: &Position) -> HashSet<Position> {
+pub fn generate_all_moves_and_castle(
+    game: &Game,
+    other_team_threat_map: &ThreatMap,
+    piece_position: &Position,
+) -> HashSet<Position> {
     if piece_position.x >= BOARD_SIZE || piece_position.y >= BOARD_SIZE {
         return HashSet::new();
     }
@@ -467,7 +478,12 @@ pub fn generate_all_moves_and_castle(game: &Game, piece_position: &Position) -> 
 
     let start_piece = game.board[piece_position.x][piece_position.y];
     if start_piece.piece == Piece::King {
-        let castle_moves = get_castle_positions(game, piece_position, start_piece.is_white);
+        let castle_moves = get_castle_positions(
+            game,
+            other_team_threat_map,
+            piece_position,
+            start_piece.is_white,
+        );
         for c_move in castle_moves {
             moves.insert(c_move);
         }
@@ -504,8 +520,8 @@ pub fn generate_valid_moves(
 ) -> HashSet<Position> {
     let mut valid_positions: HashSet<Position> = HashSet::new();
 
-    let all_positions = generate_all_moves(game, piece_position);
-    if all_positions.all_moves.len() == 0 {
+    let all_positions = generate_all_moves_and_castle(game, other_team_threat_map, piece_position);
+    if all_positions.len() == 0 {
         return valid_positions;
     }
 
@@ -515,13 +531,17 @@ pub fn generate_valid_moves(
     // 1. find king threats
     if other_team_threat_map.all_king_threats.len() > 0 {
         if piece_data.piece == Piece::King {
-            for pos in all_positions.all_moves {
-                if !is_square_color(game, &pos, is_white)
-                    && !other_team_threat_map.all_threats.contains(&pos)
-                    && !other_team_threat_map.all_king_threats_full.contains(&pos)
-                {
-                    //println!("KING: {}", print_position(pos));
-                    valid_positions.insert(pos);
+            for pos in all_positions {
+                // 1. King cant capture own color
+                // 2. King can capture the one that threatens the king, but cant capture a piece that threats it if it is also threatend
+                if !is_square_color(game, &pos, is_white) {
+                    if (is_square_color(game, &pos, !is_white)
+                        && !other_team_threat_map.all_threats.contains(&pos))
+                        || (!other_team_threat_map.all_threats.contains(&pos)
+                            && !other_team_threat_map.all_king_threats_full.contains(&pos))
+                    {
+                        valid_positions.insert(pos);
+                    }
                 }
             }
         } else {
@@ -536,7 +556,7 @@ pub fn generate_valid_moves(
             // place infront of king or capture
             // but cant move if it will reveal the king
             if !is_secondary {
-                for pos in all_positions.all_moves {
+                for pos in all_positions {
                     if !is_square_color(game, &pos, is_white)
                         && other_team_threat_map.all_king_threats.contains(&pos)
                     {
@@ -555,7 +575,7 @@ pub fn generate_valid_moves(
     if piece_data.piece != Piece::King {
         for local_list in &other_team_threat_map.all_pinned {
             if local_list.contains(piece_position) {
-                for pos in all_positions.all_moves {
+                for pos in all_positions {
                     if local_list.contains(&pos) {
                         valid_positions.insert(pos);
                     }
@@ -563,12 +583,26 @@ pub fn generate_valid_moves(
                 return valid_positions;
             }
         }
-        for pos in all_positions.all_moves {
+        for pos in all_positions {
             valid_positions.insert(pos);
         }
     } else {
-        for pos in all_positions.all_moves {
+        for pos in all_positions {
             if !other_team_threat_map.all_threats.contains(&pos) {
+                // you cant capture a pinned piece with your king
+                let mut cant_capture = false;
+                if is_square_color(game, &pos, !is_white) {
+                    for local_list in &other_team_threat_map.all_pinned {
+                        if local_list.contains(&pos) {
+                            cant_capture = true;
+                            break;
+                        }
+                    }
+                }
+                if cant_capture {
+                    continue;
+                }
+
                 valid_positions.insert(pos);
             }
         }
@@ -595,13 +629,9 @@ fn remove_castle(game: &mut Game, pos: Position) {
 /** Will go horrible wrong if input is not checked, only use if you have checked the move beforehand with valid moves
 Will return true if move is done
 */
-pub fn move_piece_unsafe(
-    game: &mut Game,
-    move_start: Position,
-    move_end: Position,
-) -> bool {
+pub fn move_piece_unsafe(game: &mut Game, move_start: Position, move_end: Position) -> bool {
     let start_piece = game.board[move_start.x][move_start.y];
-    
+
     if start_piece.piece == Piece::None {
         return false;
     }
@@ -637,7 +667,7 @@ pub fn move_piece_unsafe(
                 // captures the real pawn and not just air
                 if real_capture_unchecked.is_some() {
                     let real_capture = real_capture_unchecked.unwrap();
-                    let real_capture_piece_data = game.board[real_capture.x][real_capture.y];
+                    //let real_capture_piece_data = game.board[real_capture.x][real_capture.y];
 
                     game.board[real_capture.x][real_capture.y] = EMPTY_PEICE;
                 }
@@ -774,138 +804,4 @@ pub fn move_piece(
         flags.insert(MoveFlags::Invalid);
         return flags;
     }
-
-    /*
-    let is_white = game.is_white_to_move;
-
-    if get_promotion_pawn(game).is_some() {
-        flags.insert(MoveFlags::Invalid);
-        flags.insert(MoveFlags::InvalidWaitingForPromotion);
-        return flags;
-    }
-
-    // basic check first
-    if !is_valid_move(game, move_start, move_end) {
-        flags.insert(MoveFlags::Invalid);
-        return flags;
-    }
-
-    let start_piece = game.board[move_start.x][move_start.y];
-
-    let done_castle = try_castle(game, move_start, move_end);
-
-    let mut half_move_clock = game.half_move_clock + 1;
-    let mut en_passant_position: Option<Position> = None;
-
-    // have already done casteling, ignore the regular moves
-    if !done_castle {
-        // advanced check
-        let all_valid_moves = generate_all_moves(&game, move_start);
-        if !all_valid_moves.contains(move_end) {
-            flags.insert(MoveFlags::Invalid);
-            return flags;
-        }
-
-        let capture_piece = game.board[move_end.x][move_end.y];
-
-        // moves the piece
-        game.board[move_start.x][move_start.y] = EMPTY_PEICE;
-        game.board[move_end.x][move_end.y] = start_piece;
-
-        let other_threats = generate_all_threats(game, !is_white);
-        let king_position = find_king(&game, is_white).unwrap();
-
-        // move is invalid because it causes the king to be in check
-        if other_threats.contains(&king_position) {
-            // undo move
-            game.board[move_start.x][move_start.y] = start_piece;
-            game.board[move_end.x][move_end.y] = capture_piece;
-
-            flags.insert(MoveFlags::InvalidRevealKing);
-            flags.insert(MoveFlags::Invalid);
-            return flags;
-        }
-
-        // en passant setup
-        if start_piece.piece == Piece::Pawn {
-            let pawn_spawn = if start_piece.is_white {
-                WHITE_PAWN_Y
-            } else {
-                BLACK_PAWN_Y
-            };
-
-            if move_start.y == pawn_spawn && move_end.y == move_start.y + 2usize {
-                en_passant_position = get_position(move_start, &Vector2 { x: 0, y: 1 });
-            }
-        } else if start_piece.piece == Piece::Rook {
-            //TODO random chess add start position
-
-            //removes the castling avalibility
-            let spawn = if is_white { WHITE_SPAWN } else { BLACK_SPAWN };
-            if move_start.y == spawn {
-                if move_start.x == 0 {
-                    if is_white {
-                        game.white_castle.can_castle_queen_side = false;
-                    } else {
-                        game.black_castle.can_castle_queen_side = false;
-                    }
-                } else if move_start.x == BOARD_SIZE - 1 {
-                    if is_white {
-                        game.white_castle.can_castle_king_side = false;
-                    } else {
-                        game.black_castle.can_castle_king_side = false;
-                    }
-                }
-            }
-        }
-
-        if auto_promote {
-            promote_pawn(game, Piece::Queen);
-        }
-
-        if capture_piece.piece != Piece::None {
-            flags.insert(MoveFlags::Capture);
-
-            if capture_piece.piece == Piece::Pawn {
-                half_move_clock = 0;
-            }
-        }
-    }
-
-    if start_piece.piece == Piece::King {
-        let castle = Castle {
-            can_castle_king_side: false,
-            can_castle_queen_side: false,
-        };
-        if is_white {
-            game.white_castle = castle;
-        } else {
-            game.black_castle = castle;
-        }
-    }
-
-    // todo checkmate or tie
-
-    // update clock
-
-    let full_move_clock = if is_white {
-        game.full_move_clock
-    } else {
-        game.full_move_clock + 1
-    };
-
-    game.half_move_clock = half_move_clock;
-    game.full_move_clock = full_move_clock;
-    game.is_white_to_move = !is_white;
-    game.en_passant_position = en_passant_position;
-
-    // 50 move rule
-    if half_move_clock >= 50
-        && !(flags.contains(&MoveFlags::BlackWon) || flags.contains(&MoveFlags::WhiteWon))
-    {
-        flags.insert(MoveFlags::Tie);
-    }
-
-    flags.insert(MoveFlags::Valid);
-    flags*/
 }
